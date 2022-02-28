c83d42960c04c75a60a40d41bebec593df5209f5
pnxdxt <paul.nodet@gmail.com>
2022-02-28 21:46:19 +0100

    Clean

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,43 +1,38 @@
 import Image from 'next/image.js';
 import Link from 'next/link';
+import {HeartIcon} from '@heroicons/react/solid';
 import github from '@/public/images/github.svg';
 import discord from '@/public/images/discord.svg';
 
 const Footer = () => (
-	<footer className='flex h-40 w-full flex-row items-center justify-center bg-slate-900 shadow'>
+	<footer className='flex h-24 w-full flex-row items-center justify-center bg-slate-900 shadow'>
 		<div className='flex w-full content-center justify-between '>
 			<div className='mx-5 flex flex-col items-center justify-center '>
 				{/* TODO: add link to sponsor page */}
 				<Link href='#'>
-					<div className=' flex w-full cursor-pointer content-center  items-center justify-between px-3 '>
-						<p className='cursor-pointer border-b-2 border-solid  pb-2 pr-3 text-xl text-white'>
-							Become official ChessPecker SPONSOR
-						</p>
-						{/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
-						<p className='text-3xl'>🔥</p>
+					<div className=' flex w-full cursor-pointer content-center items-center justify-between px-3 '>
+						<HeartIcon className='h-6 w-6 text-white' />
+						<p className='ml-3 cursor-pointer text-xl text-white'>Support</p>
 					</div>
 				</Link>
 			</div>
-			<div className='mx-5 flex flex-col '>
-				<p className='order-white mb-3 cursor-pointer border-b-2 border-solid  pb-3 text-xl text-white'>
-					Bug report or ideas? 💡
-				</p>
+			<div className='mx-5 flex flex-row '>
 				<Link href='https://github.com/chesspecker'>
-					<div className=' flex w-full cursor-pointer content-center  items-center justify-between px-3 '>
-						<p className='cursor-pointer border-solid pr-3   text-xl text-white'>
+					<div className=' flex w-full cursor-pointer content-center items-center justify-between px-3 '>
+						{/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
+						<Image src={github} width={30} height={30} />
+						<p className='ml-3 cursor-pointer border-solid text-xl text-white'>
 							Github
 						</p>
-						{/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
-						<Image src={github} width={40} height={40} />
 					</div>
 				</Link>
 				<Link href='https://discord.gg/AaaAuUZK'>
 					<div className=' flex w-full cursor-pointer content-center  items-center justify-between px-3  '>
-						<p className='cursor-pointer border-solid pr-3   text-xl text-white'>
+						{/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
+						<Image src={discord} width={25} height={25} />
+						<p className='ml-3 cursor-pointer border-solid text-xl text-white'>
 							Discord
 						</p>
-						{/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
-						<Image src={discord} width={40} height={40} />
 					</div>
 				</Link>
 			</div>
