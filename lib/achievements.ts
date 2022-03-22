10508a3c9bba6562f18ffeb98c3e12e168b91944
henribruvier <henri.bruvier@gmail.com>
2022-03-22 17:12:05 +0100

    create prov achievements list

---
+++ lib/achievements.ts
@@ -0,0 +1,30 @@
+export const achievements = [
+	{
+		id: 'rabbit',
+		name: 'Rabbit',
+		description: 'series of 20 puzzles in less than 5 seconds',
+		image:
+			'http://www.lespetitslapins.fr/wp-content/uploads/2011/01/pan-pan1.jpg',
+	},
+	{
+		id: 'rabbit-master',
+		name: 'Rabbit Master',
+		description: 'series of 30 puzzles in less than 5 seconds',
+		image:
+			'http://www.lespetitslapins.fr/wp-content/uploads/2011/01/pan-pan1.jpg',
+	},
+	{
+		id: 'super-rabbit-master',
+		name: 'Super RabitMaster',
+		description: 'series of 50 puzzles in less than 5 seconds',
+		image:
+			'http://www.lespetitslapins.fr/wp-content/uploads/2011/01/pan-pan1.jpg',
+	},
+	{
+		id: 'turtle',
+		name: 'turtle',
+		description: 'series of 50 puzzles in less than 5 seconds',
+		image:
+			'http://www.lespetitslapins.fr/wp-content/uploads/2011/01/pan-pan1.jpg',
+	},
+];
