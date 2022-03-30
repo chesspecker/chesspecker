b96c0d4b8427af44c504eacbec208dc1e3aaa6f7
henribruvier <henri.bruvier@gmail.com>
2022-03-30 19:16:33 +0200

    add new achievements

--- data/achievements.ts
+++ data/achievements.ts
@@ -1,47 +1,199 @@
-import {AchievementInterface} from '@/models/types';
+import {AchievementInterface, AchivementsArgs} from '@/models/types';
+import THEMES from '@/data/themes';
 
-type Args = {
-	strikeMistakes: number;
-	strikeTime: number;
-	lastTime: number;
-};
+const getRandomInt = (max: number): number => Math.floor(Math.random() * max);
 
 export const achievements: AchievementInterface[] = [
+	// RANDOM
+	{
+		id: 'pizza',
+		name: 'Pizza',
+		description: 'Great, you won a slice of pizza...',
+		isValidated: (args: AchivementsArgs) => getRandomInt(1000) === 42,
+		image: 'http://localhost:3000/images/achievements/pizza.png',
+	},
+	{
+		id: 'plunger',
+		name: 'Toilet plunger',
+		description: 'Great, you won a toilet plunger...',
+		isValidated: (args: AchivementsArgs) => getRandomInt(1000) === 666,
+		image: 'http://localhost:3000/images/achievements/plunger.jpg',
+	},
+	{
+		id: 'unicorn',
+		name: 'Nice unicorn',
+		description: 'I love unicorns and their beautiful horns',
+		isValidated: (args: AchivementsArgs) => getRandomInt(1000) === 999,
+		image: 'http://localhost:3000/images/achievements/unicorn.png',
+	},
+	{
+		id: 'clown',
+		name: 'Funny clown',
+		description: 'Funny joke goes here',
+		isValidated: (args: AchivementsArgs) => getRandomInt(1000) === 181,
+		image: 'http://localhost:3000/images/achievements/clown.png',
+	},
+
+	// AVANCEMENT
 	{
 		id: 'first-puzzle',
 		name: 'First Puzzle',
 		description: 'You completed your first puzzle.',
-		isValidated: (args: Args) => true,
+		isValidated: (args: AchivementsArgs) => args.totalPuzzleSolved === 1,
+		image: 'http://localhost:3000/images/achievements/baby_bunny.svg', // change
+	},
+	{
+		id: 'first-set',
+		name: 'First Set',
+		description: 'You completed your first set.',
+		isValidated: (args: AchivementsArgs) => args.totalSetSolved === 1,
+		image: 'http://localhost:3000/images/achievements/baby_bunny.svg', // change
+	},
+
+	// PERFORMANCE
+	{
+		id: 'baby-bunny',
+		name: 'Baby Bunny',
+		description: 'Solve 10 consecutive puzzles in less than 5 seconds',
+		isValidated: (args: AchivementsArgs) => args.streakTime > 20,
 		image: 'http://localhost:3000/images/achievements/baby_bunny.svg',
 	},
 	{
-		id: 'rabbit',
-		name: 'Rabbit',
-		description: 'series of 20 puzzles in less than 5 seconds',
-		isValidated: (args: Args) => args.strikeTime > 20,
+		id: 'bunny',
+		name: 'Bunny',
+		description: 'Solve 25 consecutive puzzles in less than 5 seconds',
+		isValidated: (args: AchivementsArgs) => args.streakTime > 30,
 		image: 'http://localhost:3000/images/achievements/bunny.svg',
 	},
 	{
-		id: 'rabbit-master',
-		name: 'Rabbit Master',
-		description: 'series of 30 puzzles in less than 5 seconds',
-		isValidated: (args: Args) => args.strikeTime > 30,
+		id: 'king-bunny',
+		name: 'King Bunny',
+		description: 'Solve 50 consecutive puzzles in less than 5 seconds',
+		isValidated: (args: AchivementsArgs) => args.streakTime > 50,
+		image: 'http://localhost:3000/images/achievements/king_bunny.svg',
+	},
+
+	{
+		id: 'baby-turtle',
+		name: 'Baby turtle',
+		description: 'Solve a puzzle after 2 minutes',
+		isValidated: (args: AchivementsArgs) => args.completionTime > 120,
 		image: 'http://localhost:3000/images/achievements/king_bunny.svg',
 	},
 	{
-		id: 'super-rabbit-master',
-		name: 'Super RabitMaster',
-		description: 'series of 50 puzzles in less than 5 seconds',
-		isValidated: (args: Args) => args.strikeTime > 50,
-		image:
-			'http://www.lespetitslapins.fr/wp-content/uploads/2011/01/pan-pan1.jpg',
+		id: 'turtle',
+		name: 'turtle',
+		description: 'Solve a puzzle after 5 minutes',
+		isValidated: (args: AchivementsArgs) => args.completionTime > 300,
+		image: 'http://localhost:3000/images/achievements/king_bunny.svg',
 	},
 	{
-		id: 'turlle',
-		name: 'Turtle',
-		description: 'more than 2 minutes to solve a puzzle',
-		isValidated: (args: Args) => args.lastTime > 120, // sans erreurs ?
-		image:
-			'http://www.lespetitslapins.fr/wp-content/uploads/2011/01/pan-pan1.jpg',
+		id: 'king-turtle',
+		name: 'King turtle',
+		description: 'Solve a puzzle after 15 minutes',
+		isValidated: (args: AchivementsArgs) => args.completionTime > 900,
+		image: 'http://localhost:3000/images/achievements/king_bunny.svg',
+	},
+
+	{
+		id: 'baby-watchmaker',
+		name: 'Baby watchmaker',
+		description: 'Solve 20 consecutive puzzles with no mistakes',
+		isValidated: (args: AchivementsArgs) => args.streakMistakes > 20,
+		image: 'http://localhost:3000/images/achievements/baby_watchmaker.svg',
+	},
+	{
+		id: 'watchmaker',
+		name: 'watchmaker',
+		description: 'Solve 30 consecutive puzzles with no mistakes',
+		isValidated: (args: AchivementsArgs) => args.streakMistakes > 30,
+		image: 'http://localhost:3000/images/achievements/watchmaker.svg',
+	},
+	{
+		id: 'king-watchmaker',
+		name: 'King watchmaker',
+		description: 'Solve 50 consecutive puzzles with no mistakes',
+		isValidated: (args: AchivementsArgs) => args.streakMistakes > 50,
+		image: 'http://localhost:3000/images/achievements/king_watchmaker.svg',
+	},
+
+	{
+		id: 'baby-goat',
+		name: 'Baby goat',
+		description: 'Solve a puzzle after more than 5 mistakes',
+		isValidated: (args: AchivementsArgs) => args.completionTime > 900,
+		image: 'http://localhost:3000/images/achievements/king_bunny.svg',
+	},
+	{
+		id: 'goat',
+		name: 'Goat',
+		description: 'Solve a puzzle after more than 15 mistakes',
+		isValidated: (args: AchivementsArgs) => args.completionTime > 900,
+		image: 'http://localhost:3000/images/achievements/king_bunny.svg',
+	},
+	{
+		id: 'king-goat',
+		name: 'King goat',
+		description: 'Solve a puzzle after more than 30 mistakes',
+		isValidated: (args: AchivementsArgs) => args.completionTime > 900,
+		image: 'http://localhost:3000/images/achievements/king_bunny.svg',
+	},
+	//DURATION
+
+	{
+		id: 'baby-chessaolic',
+		name: 'Baby chessaolic',
+		description: 'Play more than 15 mn in last 7 days',
+		isValidated: (args: AchivementsArgs) => args.streakDays > 7,
+		image: 'http://localhost:3000/images/achievements/king_bunny.svg',
+	},
+	{
+		id: 'chessaolic',
+		name: 'Chessaolic',
+		description: 'Play more than 15 mn in last 15 days',
+		isValidated: (args: AchivementsArgs) => args.streakDays > 15,
+		image: 'http://localhost:3000/images/achievements/king_bunny.svg',
+	},
+	{
+		id: 'king-chessaolic',
+		name: 'King chessaolic',
+		description: 'Play more than 15 mn in last 30 days',
+		isValidated: (args: AchivementsArgs) => args.streakDays > 30,
+		image: 'http://localhost:3000/images/achievements/king_bunny.svg',
+	},
+	{
+		id: 'revenant',
+		name: 'Revenant',
+		description: 'Come back after more than 30 days of inactivity',
+		isValidated: (args: AchivementsArgs) =>
+			(Date.now() - args.lastVisit) / 1000 > 2592000, //30 jours en secondes
+		image: 'http://localhost:3000/images/achievements/king_bunny.svg',
 	},
 ];
+
+for (const theme of THEMES) {
+	achievements.push({
+		id: theme.id,
+		name: `National master of ${theme.title}`,
+		description: `Your are now national master of ${theme.description}`,
+		isValidated: (args: AchivementsArgs) =>
+			args.theme.id === theme.id && args.theme.totalPuzzleSolved === 500,
+		image: 'http://localhost:3000/images/achievements/king_bunny.svg',
+	});
+	achievements.push({
+		id: theme.id,
+		name: `International master of ${theme.title}`,
+		description: `Your are now international master of ${theme.description}`,
+		isValidated: (args: AchivementsArgs) =>
+			args.theme.id === theme.id && args.theme.totalPuzzleSolved === 1000,
+		image: 'http://localhost:3000/images/achievements/king_bunny.svg',
+	});
+	achievements.push({
+		id: theme.id,
+		name: `Great-master of ${theme.title}`,
+		description: `Your are now great master of ${theme.description}`,
+		isValidated: (args: AchivementsArgs) =>
+			args.theme.id === theme.id && args.theme.totalPuzzleSolved === 1500,
+		image: 'http://localhost:3000/images/achievements/king_bunny.svg',
+	});
+}
