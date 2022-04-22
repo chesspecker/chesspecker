e7ef6b2ec92bc39c9c0a70cbdea5f155f8352a01
henribruvier <henri.bruvier@gmail.com>
2022-04-22 19:13:31 +0200

    update user page

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -28,7 +28,11 @@ const Profile = ({user}) => {
 		<div className='mt-32 flex min-h-screen w-screen flex-col px-10 text-slate-800'>
 			<div className='flex items-center'>
 				<p className=' mr-5 text-6xl text-white'>{user.username}</p>
-				<ButtonLink href='/sponsor'>Become sponsor </ButtonLink>
+				{user?.isSponsor ? (
+					<p className='text-white'>Official Sponsor</p>
+				) : (
+					<ButtonLink href='/sponsor'>Become sponsor </ButtonLink>
+				)}
 			</div>
 
 			<div className='borded-white m-2 mt-6 min-h-[5rem] rounded-xl border p-2'>
@@ -47,6 +51,9 @@ const Profile = ({user}) => {
 							</div>
 						))}
 				</div>
+			</div>
+			<div className='flex w-1/3'>
+				<ButtonLink href='/sponsor'>Manage subscription</ButtonLink>
 			</div>
 		</div>
 	);
