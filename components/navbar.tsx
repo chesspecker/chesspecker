14cee6fbe53baeca6ca8087230201711c4bdca3a
pnxdxt <paul.nodet@gmail.com>
2022-05-07 18:08:24 +0200

    Correct user page link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -37,10 +37,10 @@ const Navbar = () => {
 				</div>
 				<div className='mr-8 self-center text-lg text-white'>
 					<div className='flex'>
-						<Link passHref href={`/user/${user?._id.toString()}`}>
+						<Link passHref href={'/user/'}>
 							<a className='mr-5 flex items-center justify-center'>
+								{user?.isSponsor && <span>👑&nbsp;</span>}
 								{user?.username}
-								{user?.isSponsor && <span>&nbsp;👑</span>}
 							</a>
 						</Link>
 						<Link passHref href='/api/auth/logout'>
