23e1e663154f7e3a8b9a982ee0b669aa376519d1
pnxdxt <paul.nodet@gmail.com>
2022-05-08 09:09:57 +0200

    fix bottom tab

--- components/mobile-assets/bottom-tab.tsx
+++ components/mobile-assets/bottom-tab.tsx
@@ -1,40 +1,34 @@
-import {useState, useEffect} from 'react';
 import {UserIcon, HomeIcon, LightningBoltIcon} from '@heroicons/react/solid';
 import Link from 'next/link';
-import {UserInterface} from '@/types/models';
-import useUser from '@/hooks/use-user';
 
-const BottomTab = () => {
-	const [user, setUser] = useState<UserInterface>();
-	const data = useUser();
-
-	useEffect(() => {
-		if (!data) return;
-		setUser(data.user);
-	}, [data]);
-	return (
-		<div className='bg-white fixed bottom-0 safe-bottom pt-2 min-h-[4rem] w-full sm:hidden flex justify-around items-center'>
-			<Link passHref href={`/achievements/${user?._id.toString()}`}>
+const BottomTab = () => (
+	<div className='bg-white fixed bottom-0 safe-bottom pt-2 min-h-[4rem] w-full sm:hidden flex justify-around items-center'>
+		<Link href='/achievements'>
+			<a>
 				<div className='border-b-2 border-white hover:border-sky-700 focus:border-sky-700'>
 					<LightningBoltIcon
 						className='w-8 h-8 text-sky-700'
 						aria-hidden='true'
 					/>
 				</div>
-			</Link>
+			</a>
+		</Link>
 
-			<Link passHref href='/dashboard'>
+		<Link href='/dashboard'>
+			<a>
 				<div className='border-b-2 border-white hover:border-sky-700 focus:border-sky-700'>
 					<HomeIcon className='w-8 h-8 text-sky-700' aria-hidden='true' />
 				</div>
-			</Link>
-			<Link passHref href={`/user/${user?._id.toString()}`}>
+			</a>
+		</Link>
+		<Link href='/user'>
+			<a>
 				<div className='border-b-2 border-white hover:border-sky-700 focus:border-sky-700'>
 					<UserIcon className='w-8 h-8 text-sky-700' aria-hidden='true' />
 				</div>
-			</Link>
-		</div>
-	);
-};
+			</a>
+		</Link>
+	</div>
+);
 
 export default BottomTab;
