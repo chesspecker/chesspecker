de9655e3871331f20e7db2cb30645fac9007f80f
pnxdxt <paul.nodet@gmail.com>
2022-05-10 10:30:43 +0200

    lint

--- layouts/main.tsx
+++ layouts/main.tsx
@@ -1,5 +1,5 @@
 import {useAtom} from 'jotai';
-import {darkModeµ} from '../lib/atoms';
+import {darkModeµ} from '@/lib/atoms';
 import Footer from '@/components/footer';
 import BottomTab from '@/components/mobile-assets/bottom-tab';
 import Navbar from '@/components/navbar';
@@ -8,7 +8,7 @@ const MainLayout = ({children}: {children: React.ReactNode}) => {
 	const [isDarkMode] = useAtom(darkModeµ);
 
 	return (
-		<main className={` ${isDarkMode && 'dark'}`}>
+		<main className={`${isDarkMode && 'dark'}`}>
 			<div className='relative flex flex-col items-center justify-between h-full min-h-screen overflow-y-scroll font-sans dark:text-white text-sky-900 disable-scrollbars bg-gradient-to-t dark:from-slate-900 from-white to-white dark:to-sky-700'>
 				<Navbar />
 				<div className='w-full h-full'>{children}</div>
