0aac732ecd41925ead0b46254688c1645519b2df
pnxdxt <paul.nodet@gmail.com>
2022-05-12 15:02:36 +0200

    Fix solution btn

--- components/play/right-bar/solution.tsx
+++ components/play/right-bar/solution.tsx
@@ -7,8 +7,8 @@ import {playµ} from '@/lib/atoms';
 type Props = {answer: string};
 
 const defaultClasses =
-	'mx-auto md:mx-2 w-36 rounded-md bg-slate-800 dark:bg-white leading-8 mt-0 ml-2';
-const disabledClasses = `block cursor-default text-white dark:text-sky-800 self-center border border-none border-transparent px-2.5 py-2 text-center font-sans text-sm md:text-lg font-bold shadow-sm ${defaultClasses}`;
+	'mx-auto md:mx-2 w-36 rounded-md bg-slate-800 dark:bg-white leading-8 mt-0 ml-2 font-bold font-sans text-sm md:text-lg px-2.5 py-2';
+const disabledClasses = `block cursor-default text-white dark:text-sky-800 self-center border border-none border-transparent text-center shadow-sm ${defaultClasses}`;
 
 const Solution = ({answer}: Props) => {
 	const [isSolutionClicked, setIsSolutionClicked] = useAtom(playµ.solution);
@@ -21,9 +21,19 @@ const Solution = ({answer}: Props) => {
 	}, [time, updateTimer]);
 
 	if (timer.value < 6)
-		return <div className={disabledClasses}>NO CHEATING</div>;
+		return <div className={disabledClasses}>SOLUTION IN {6 - timer.value}</div>;
 
-	if (isComplete) return null;
+	if (isComplete)
+		return (
+			<Button
+				className={disabledClasses}
+				onClick={() => {
+					('');
+				}}
+			>
+				VIEW SOLUTION
+			</Button>
+		);
 
 	if (isSolutionClicked)
 		return <span className={disabledClasses}>{answer}</span>;
