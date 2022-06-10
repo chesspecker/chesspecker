53dc96400a4d076d70f263f7e05c10e0d0df8f43
henribruvier <henri.bruvier@gmail.com>
2022-06-10 11:39:47 +0200

    add color on letter

--- components/play/left-bar/index.tsx
+++ components/play/left-bar/index.tsx
@@ -21,10 +21,18 @@ const LeftBar = ({stat}: Props) => {
 	const gradeDiff = gradeLast ? gradeCurrent - gradeLast : 0;
 	const timeDiff = timeLast ? timeCurrent - timeLast : 0;
 
+	let letterClassname = 'text-red-500';
+	if (parseGrade[gradeCurrent] === 'A') letterClassname = 'text-green-500';
+	if (parseGrade[gradeCurrent] === 'B' || parseGrade[gradeCurrent] === 'C')
+		letterClassname = 'text-orange-500';
+
 	return (
 		<div className='text-md mx-auto hidden md:block h-fit w-36 cursor-default rounded-md border border-transparent bg-slate-800 dark:bg-white py-2 px-2.5 text-center font-sans font-bold leading-8 text-white dark:text-sky-800 shadow-sm backdrop-blur-lg backdrop-filter md:mx-2'>
 			<span>Last puzzle: </span>
-			<span>grade: {parseGrade[gradeCurrent]}</span>
+			<span>
+				grade:{' '}
+				<span className={letterClassname}> {parseGrade[gradeCurrent]}</span>
+			</span>
 
 			{gradeDiff !== 0 && (
 				<p
