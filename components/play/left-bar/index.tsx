f32e63b0b43528faf9cd4f37982d8b87c6291fd7
pnxdxt <paul.nodet@gmail.com>
2022-06-10 14:03:12 +0200

    Change grades colors

--- components/play/left-bar/index.tsx
+++ components/play/left-bar/index.tsx
@@ -14,6 +14,12 @@ type Props = {
 	stat: Stat;
 };
 
+const getGradeColor = (grade: number) => {
+	if (grade < 3) return `text-red-500`;
+	if (grade < 5) return `text-orange-500`;
+	if (grade < 7) return `text-green-500`;
+};
+
 const LeftBar = ({stat}: Props) => {
 	if (!stat)
 		return <div className='hidden w-36 px-2.5 md:mx-2 md:invisible md:block' />;
@@ -21,17 +27,14 @@ const LeftBar = ({stat}: Props) => {
 	const gradeDiff = gradeLast ? gradeCurrent - gradeLast : 0;
 	const timeDiff = timeLast ? timeCurrent - timeLast : 0;
 
-	let letterClassname = 'text-red-500';
-	if (parseGrade[gradeCurrent] === 'A') letterClassname = 'text-green-500';
-	if (parseGrade[gradeCurrent] === 'B' || parseGrade[gradeCurrent] === 'C')
-		letterClassname = 'text-orange-500';
-
 	return (
 		<div className='text-md mx-auto hidden md:block h-fit w-36 cursor-default rounded-md border border-transparent bg-slate-800 dark:bg-white py-2 px-2.5 text-center font-sans font-bold leading-8 text-white dark:text-sky-800 shadow-sm backdrop-blur-lg backdrop-filter md:mx-2'>
 			<span>Last puzzle: </span>
 			<span>
 				grade:{' '}
-				<span className={letterClassname}> {parseGrade[gradeCurrent]}</span>
+				<span className={getGradeColor(gradeCurrent)}>
+					{parseGrade[gradeCurrent]}
+				</span>
 			</span>
 
 			{gradeDiff !== 0 && (
