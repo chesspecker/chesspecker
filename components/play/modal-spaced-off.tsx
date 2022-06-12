9adc84837093ff6fd08e2eb8c64baf8b8b859da3
pnxdxt <paul.nodet@gmail.com>
2022-06-12 23:18:43 +0200

    Correct minor typo. Fix #60

--- components/play/modal-spaced-off.tsx
+++ components/play/modal-spaced-off.tsx
@@ -61,7 +61,7 @@ const ModalSpacedOff = ({onClick, isOpen = false, hide}: Props) => {
 								className='inline-flex justify-center px-4 py-2 ml-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2'
 								onClick={wrapOnClick}
 							>
-								Desactivate
+								Deactivate
 							</Button>
 						</>
 					)}
