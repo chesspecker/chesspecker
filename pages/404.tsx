b5f2f68adb2d9d8e96791a7b13de051ec1ace6d5
pnxdxt <paul.nodet@gmail.com>
2022-07-14 15:53:27 +0200

    fix(typo)

--- pages/404.tsx
+++ pages/404.tsx
@@ -7,7 +7,7 @@ const LostPage = () => {
 	return (
 		<div className='flex flex-col items-center justify-center h-screen m-0 '>
 			<h1 className='p-5 mx-auto mt-8 mb-6 font-sans text-3xl font-bold text-center '>
-				Looks like you are lost..
+				Looks like you are lost...
 			</h1>
 			<div className='w-full mx-0 my-3 text-center '>
 				<Link href='/dashboard'>
