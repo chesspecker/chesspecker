4a0b08f48f45ebf4e046826b7f4927b941bd9dea
pnxdxt <paul.nodet@gmail.com>
2022-07-27 22:19:57 +0200

    chore(env): update env variable and add type checking

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@
 const plugin = require('tailwindcss/plugin');
 const defaultTheme = require('tailwindcss/defaultTheme');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
 		'./components/**/*.{js,ts,jsx,tsx}',
